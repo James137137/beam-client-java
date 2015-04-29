907219e1b061ae2e89af8c8c27fb02f97de7e0fc
Taylor Blau <me@ttaylorr.com>
2015-04-28 20:04:59 -0400

    Fix NoSuchElement exception from Queue#element call when empty

--- src/main/java/pro/beam/api/util/AbstractQueueWorker.java
+++ src/main/java/pro/beam/api/util/AbstractQueueWorker.java
@@ -15,7 +15,7 @@ public abstract class AbstractQueueWorker<T> implements Runnable {
     @Override public void run() {
         T next;
         while (this.shouldWork()) {
-            if ((next = this.queue.element()) != null) {
+            if ((next = this.queue.peek()) != null) {
                 boolean shouldRemove = this.applicant.apply(next);
                 if (shouldRemove) {
                     this.notifyWorkOn(next);
